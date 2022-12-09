08ca9a924f75c14a5c6e4969ec61a90c6c766a87
harshavinnu <119720338+harshavinnu@users.noreply.github.com>
2022-12-09 11:01:07 +0530

    #include<stdio.h> int main() {     long long int a;     double b,c;     scanf("%lld",&a);     b=6*a*a;     c=a*a*a;     printf("Surface area = %.2f and Volume = %.2f",b,c); }

---
+++ surface_area_and_volume_of_a_cube.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main()
+{       
+    long long int a;
+    double b,c;   
+    scanf("%lld",&a); 
+    b=6*a*a;    
+    c=a*a*a;   
+    printf("Surface area = %.2f and Volume = %.2f",b,c);
+}
+    
+    
