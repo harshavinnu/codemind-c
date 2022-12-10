88803a1de74ae459efb41ca45b044fe1c54a1721
harshavinnu <119720338+harshavinnu@users.noreply.github.com>
2022-12-10 11:22:07 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d%d",&x,&y,&z);     if(x>=y*30)     {         printf("YES");         }         else         {             printf("NO");         } }

---
+++ Expenditure.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z; 
+    scanf("%d%d%d",&x,&y,&z);
+    if(x>=y*30) 
+    {
+        printf("YES");
+        } 
+        else
+        {
+            printf("NO");
+        }
+}
