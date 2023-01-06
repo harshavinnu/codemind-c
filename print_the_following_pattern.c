f2df35ba8bb305c7b018bf475871e755c93a939c
harshavinnu <119720338+harshavinnu@users.noreply.github.com>
2023-01-06 17:33:51 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)//row     {         for(j=n;j>=1;j--)//column         {             printf("%d ",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,11 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=n;i>=1;i--)//row
     {
-        for(j=1;j<=i;j++)
+        for(j=n;j>=1;j--)//column
         {
-            printf("%d",j);
+            printf("%d ",j);
         }
         printf("
 ");
